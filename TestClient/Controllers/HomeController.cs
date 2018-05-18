bc5a684ad96435b92878dc4360ea7a1547273821
Henry Wang <henry@chi.swan.ac.uk>
2018-05-18 15:46:15 +0100

    Add project files.

---
+++ TestClient/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestClient.Models;
+
+namespace TestClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://testapi")
+            };
+
+            var list = new List<string> { "1" , "2" , "3" };
+
+            var response = await httpClient.PostAsJsonAsync("api/values", list);
+            response.EnsureSuccessStatusCode();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
